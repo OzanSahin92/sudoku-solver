83d0e7d396f0b4848ec656a2b03c6c36224bf786
OzanSahin92 <ozansahin92@gmail.com>
2021-12-28 15:34:04 +0100

    sudoku solver script finished

--- src/sudoku_solver.py
+++ src/sudoku_solver.py
@@ -2,17 +2,10 @@
 # coding: utf-8
 
 import numpy as np
-from numba import njit
-import sys
 
 
-print(sys.getrecursionlimit())
-sys.setrecursionlimit(48000)
-
-# validity check sudoku
-@njit
-def check_pos_val(y, x, val):
-    global field
+# checks if val is possible on positions y,x on the sudoku field
+def check_pos_val(field, y, x, val):
     # check row of pos
     for i in range(9):
         if field[y][i] == val:
@@ -32,23 +25,40 @@ def check_pos_val(y, x, val):
     return True
 
 
-@njit
-def solve():
-    global field
+# solve function uses backtracking recursively
+def solve(field):
     for y in range(9):
         for x in range(9):
             if field[y][x] == 0:
                 for val in range(1, 10):
-                    if check_pos_val(y, x, val):
-                        field[y][x] == val
-                        solve()
-                        field[y][x] == 0
+                    # Check values 1 to 9 if they are possible on position y,x
+                    if check_pos_val(field, y, x, val):
+                        # if the check was successful field square is set as val
+                        field[y][x] = val
+                        # solve function is recursively called here to check the next empty square in the sudoku field
+                        solve(field)
+                        # if all checks went through without a valid value to set in the position y,x, the original
+                        # field is set to zero again and a new value is checked based on the for loop (backtracking)
+                        field[y][x] = 0
                 return
     print(field)
 
 
-path_to_sudoku_field = '../data/sudoku_field.txt'
+def main():
+    path_to_sudoku_field = '../data/sudoku_field.txt'
 
-field = np.loadtxt(path_to_sudoku_field, dtype=int)
+    sudoku_field = np.loadtxt(path_to_sudoku_field, dtype=int)
 
-solve()
+    print('Solving the following Sudoku field: \n')
+
+    print(sudoku_field)
+
+    print('\n\n')
+
+    print('Solved Sudoku field: \n')
+
+    solve(sudoku_field)
+
+
+if __name__ == "__main__":
+    main()
